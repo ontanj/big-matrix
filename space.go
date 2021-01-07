12bed0b60f47db5c8099e8bd65b5cdbb3cd7caf9
Anton Jeppsson <anton@ontanj.se>
2021-01-07 17:47:39 +0100

    Fix typo

--- space.go
+++ space.go
@@ -15,6 +15,6 @@ type Space interface {
     Scale(spaced interface{}, factor interface{}) (product interface{}, err error)
 
     // return true if this space (matrix) consist of scalar factors
-    // e.i. if Scale are to be used in matrix multiplication
+    // i.e. if Scale are to be used in matrix multiplication
     Scalarspace() bool
 }
